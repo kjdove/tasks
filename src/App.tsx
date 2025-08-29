a7d6aacaf84b21f4f77411199fa88d21117960b2
kjdove <kjdove@udel.edu>
2025-08-29 12:29:42 -0400

    hello world commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
             <p>Kacey Dove</p>
+            <p>Hello World</p>
         </div>
     );
 }
