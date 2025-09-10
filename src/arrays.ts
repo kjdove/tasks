3291827abfb584340f0a9b133e1027f70eddbac0
Kacey Dove <kjdove@udel.edu>
2025-09-10 10:44:36 -0400

    finished tripleNumbers

--- src/arrays.ts
+++ src/arrays.ts
@@ -23,7 +23,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    let retArr: number[] = numbers.map((num: number): number => num * 3);
+    return retArr;
 }
 
 /**
