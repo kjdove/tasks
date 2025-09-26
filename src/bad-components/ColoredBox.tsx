0051b2c93a617eca97b44bef8614b240b1562afb
Kacey Dove <kjdove@udel.edu>
2025-09-26 12:32:12 -0400

    completed ColoredBox

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -2,46 +2,34 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export const COLORS = ["red", "blue", "green"];
-const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): React.JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    return (
-        <Button
-            onClick={() => {
-                setColorIndex((1 + colorIndex) % COLORS.length);
-            }}
-        >
-            Next Color
-        </Button>
-    );
-}
-
-function ColorPreview(): React.JSX.Element {
-    return (
-        <div
-            data-testid="colored-box"
-            style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
-                display: "inline-block",
-                verticalAlign: "bottom",
-                marginLeft: "5px",
-            }}
-        ></div>
-    );
-}
+/
 
 export function ColoredBox(): React.JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(0);
+
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
-            <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
-            </div>
+            <span>The current color is: {COLORS[colorIndex]}</span>
+            <Button
+                onClick={() => {
+                    setColorIndex((1 + colorIndex) % COLORS.length);
+                }}
+            >
+                Next Color
+            </Button>
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: COLORS[colorIndex],
+                    display: "inline-block",
+                    verticalAlign: "bottom",
+                    marginLeft: "5px",
+                }}
+            ></div>
         </div>
     );
 }
